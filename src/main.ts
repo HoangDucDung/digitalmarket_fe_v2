47a0ca766c0ecf313b6b3cdfa45fe68aea4978b3
Dung1110 <105488027+HoangDucDung@users.noreply.github.com>
2026-03-03 00:11:19 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 import './assets/styles/main.scss'
-
+ 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
